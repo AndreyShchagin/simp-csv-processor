37e2de3d5b663f1997c1dc87a1be8d57c263e4af
Andrey Shchagin <andrey.shchagin@gmail.com>
2019-05-25 17:29:58 +0200

    SIMP: Error handling on main class + improved arg parsing

--- src/main/kotlin/com/symp/csv/Server.kt
+++ src/main/kotlin/com/symp/csv/Server.kt
@@ -8,6 +8,7 @@ import org.apache.commons.cli.*
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import java.lang.Exception
+import java.lang.NumberFormatException
 import java.net.ServerSocket
 import kotlin.concurrent.thread
 
@@ -17,23 +18,23 @@ fun main(args: Array<String>) {
     val options = Options().addOption("p", "port", true, "TCP port to listen on")
 
     try {
-        val line = DefaultParser().parse(options, args)
-        if(args.isEmpty())
+        if (args.isEmpty())
             throw ParseException("No arguments")
-        if (line.hasOption("port")) {
-            // print the value of block-size
-            val server = ServerSocket( if (line.getOptionValue("port").isNotBlank())  line.getOptionValue("port").toInt() else DEFAULT_SERVER_PORT )
-            log.info("Server is running on port ${server.localPort}")
+        val line = DefaultParser().parse(options, args)
+        val server = ServerSocket(if (line.getOptionValue("port").isNullOrBlank()) DEFAULT_SERVER_PORT else line.getOptionValue("port").toInt() )
+        log.info("Server is running on port ${server.localPort}")
 
-            while (true) {
-                val inboundConnection = server.accept()
-                log.info("Inbound connection: ${inboundConnection.inetAddress.hostAddress}")
+        while (true) {
+            val inboundConnection = server.accept()
+            log.info("Inbound connection: ${inboundConnection.inetAddress.hostAddress}")
 
-                thread { InboundCsvProcessor(inboundConnection).process() }
-            }
+            thread { InboundCsvProcessor(inboundConnection).process() }
         }
     } catch (err: Exception) {
-        HelpFormatter().printHelp(APP_NAME, options)
+        when(err) {
+            is NumberFormatException, is ParseException ->{HelpFormatter().printHelp(APP_NAME, options)}
+            else -> log.error("Error when starting the server",err)
+        }
     }
 }
 
